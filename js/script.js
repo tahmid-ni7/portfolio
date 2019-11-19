3abbdd54a41e06c805a7b458eb1045c14449f145
Tahmid Nishat <tahmid.ni7@gmail.com>
2019-11-19 14:34:28 +0600

    Rearrange projects and add custom script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function () {
-   
+    //for finding the footer current year
+    $('#year').text(new Date().getFullYear());
+
+    //For hiding collapse
+    $('.port-item').click(function () {
+        $('.collapse').collapse('hide');
+    });
 });
